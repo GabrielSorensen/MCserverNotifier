6a47ec1a2004cba39ecfe67cfe87299245a0ef29
Gabe Sorensen <Epicmonkie@gmail.com>
2014-09-14 19:02:46 -0600

    Create Player.java

---
+++ source/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+        private String name;
+        private String id;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getId() {
+            return id;
+        }
+        
+    }
